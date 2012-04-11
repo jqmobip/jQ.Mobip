da735ebad7d9d17e5fd1f2b14720d421ef20b844
liwei <style.liwei@gmail.com>
2012-04-11 21:05:37 +0800

    修改REAME

--- myPlugins/selectpage/jq.selectpage.js
+++ myPlugins/selectpage/jq.selectpage.js
@@ -1,6 +1,89 @@
 ﻿/**
  * @author liwei
  */
+/**
+ * @author liwei
+ */
+var _fightsCos = [{
+	"key" : "",
+	"content" : "不限"
+}, {
+	"key" : "CA",
+	"content" : "中国国航"
+}, {
+	"key" : "MU",
+	"content" : "东方航空"
+}, {
+	"key" : "CZ",
+	"content" : "南方航空"
+}, {
+	"key" : "HU",
+	"content" : "海南航空"
+}, {
+	"key" : "3U",
+	"content" : "四川航空"
+}, {
+	"key" : "SC",
+	"content" : "山东航空"
+}, {
+	"key" : "VD",
+	"content" : "河南航空"
+}, {
+	"key" : "NS",
+	"content" : "河北航空"
+}, {
+	"key" : "FM",
+	"content" : "上海航空"
+}, {
+	"key" : "JD",
+	"content" : "首都航空"
+}, {
+	"key" : "0Q",
+	"content" : "重庆航空"
+}, {
+	"key" : "GS",
+	"content" : "天津航空"
+}, {
+	"key" : "MF",
+	"content" : "厦门航空"
+}, {
+	"key" : "ZH",
+	"content" : "深圳航空"
+}, {
+	"key" : "EU",
+	"content" : "成都航空"
+}, {
+	"key" : "9C",
+	"content" : "春秋航空"
+}, {
+	"key" : "8L",
+	"content" : "祥鹏航空"
+}, {
+	"key" : "PN",
+	"content" : "西部航空"
+}, {
+	"key" : "KN",
+	"content" : "中国联合航空"
+}, {
+	"key" : "CN",
+	"content" : "大新华航空"
+}, {
+	"key" : "IJ",
+	"content" : "长城航空"
+}, {
+	"key" : "HO",
+	"content" : "吉祥航空"
+}, {
+	"key" : "G5",
+	"content" : "华夏航空"
+}, {
+	"key" : "KY",
+	"content" : "昆明航空"
+}, {
+	"key" : "JR",
+	"content" : "幸福航空"
+}];
+
 var handleEve;
 (function($) {
 	$.fn["selectpage"] = function(opts, hf) {
@@ -39,33 +122,27 @@ var handleEve;
 
 			try {
 				var that = this;
-				var markStart = '<div id="jq_selectpage" title="" data-footer="none" class="panel"><ul>';
+				var markStart = '<div id="jq_selectpage" title="选择航空公司" data-footer="none" class="panel"><ul>';
 				var markEnd = "</ul></div>";
 				var markup;
 				markup = $(markStart + markEnd);
 
 				if(opts != null && typeof opts == "object") {
-					var th = that;
+
 					var container = $(markup.children().get());
 					for( i = 0; i < opts.length; i++) {
-						var td = th;
 						var key = opts[i].key;
 						var content = opts[i].content;
 						var item = $('<li><a href="javascript:;" onclick=getValue("' + key + '","' + content + '")>' + content + '</a></li>');
 						container.append(item);
-
 					}
 				}
-				getValue = function(k, c) {
-					that.itemValue = k;
-					that.itemText = c;
-					handleEve(k, c);
-				};
+
 				$(elID).find("#jq_selectpage").remove();
 				selectpageEl = $(elID).append(markup);
 				$("#jq_selectpage ul").scroller({});
 				$.ui.loadContent("#jq_selectpage");
-				markup.on("click", "a", function() {
+				markup.on("click", "a",function(){
 					that.hidePage();
 				});
 
@@ -80,7 +157,12 @@ var handleEve;
 			}
 		};
 
+		getValue = function(k, c) {
+			handleEve(k,c);
+		};
+		
 		selectpage.prototype = {
+			
 			activePage : null,
 			hidePage : function() {
 				var that = this;
@@ -98,5 +180,6 @@ var handleEve;
 		};
 
 		return selectpage;
+		
 	})();
 })(jq);
